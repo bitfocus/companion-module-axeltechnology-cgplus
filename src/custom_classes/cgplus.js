fb659ba9c1c645fd4ed40f021a451e6999c2e397
AxelTechnology <development@axeltechnology.com>
2024-03-12 11:19:59 +0100

    Fixed Loggin Issue and other bug fixes

--- src/custom_classes/cgplus.js
+++ src/custom_classes/cgplus.js
@@ -8,35 +8,102 @@ import('node-fetch').then((module) => {
 let url
 let urlSuffix = 'http://'
 let format = 'format=json'
-
+//----------------------------------------------------------------------------------------------------
 //Class used to work whit the cg-plus api's
 //----------------------------------------------------------------------------------------------------
+
 class CGPlus {
+
 	//------------------------------------------------------------------------------------------------
 	//  the constructor uses the Ip, and url port specified by the user to build the
 	//  endpoint
 	//------------------------------------------------------------------------------------------------
-	constructor(CGPlusIp, CGPlusPort) {
-		this.CGPlusUrl = urlSuffix + CGPlusIp + ':' + CGPlusPort + '/CGPlus'
+
+	constructor(CGPlusIp, CGPlusPort, LogManager) 
+	{
+		this.CGPlusIp = CGPlusIp;
+		this.CGPlusPort = CGPlusPort;
+		this.LogManager = LogManager;
+		//On construction we check if we have a configuration (we'll call log if not for feedback)
+		//console.log("CGPlusIp", CGPlusIp);
+		//console.log("CGPlusPort", CGPlusPort);
+	
+		if (CGPlusIp !== null && CGPlusIp !== undefined && CGPlusPort !== null && CGPlusPort !== undefined && CGPlusIp !== '' && CGPlusPort !== '') {
+			// Construct the URL only if both IP and Port are not empty or null
+			this.CGPlusUrl = urlSuffix + CGPlusIp + ':' + CGPlusPort + '/CGPlus';
+		} else {
+			// Log a warning if either IP or Port is empty or null
+			this.LogManager('warn', 'Incomplete configuration: IP or Port is missing');
+		}   
 	}
 
 	//------------------------------------------------------------------------------------------------
 	//  function made to generate the endpoint url, since most of the url are the same for all api's
 	//------------------------------------------------------------------------------------------------
 
-	getUrl(api, type) {
-		if (type == 'data_p') {
+	getUrl(api, type) 
+	{
+		if (type == 'data_p') 
+		{
 			return (url = this.CGPlusUrl + '/REST/' + api + '&' + format)
-		} else if (type == 'data_no_p') {
+		} 
+		else if (type == 'data_no_p') 
+		{
 			return (url = this.CGPlusUrl + '/REST/' + api + '?' + format)
-		} else if (type == 'command') {
+		} 
+		else if (type == 'command') 
+		{
 			return (url = this.CGPlusUrl + '/REST/' + api)
-		}else if (type == "UI"){
+		}
+		else if (type == "UI")
+		{
 			return (url = this.CGPlusUrl + '/REST/UI/' + api)
-		}else if (type == "UI_D"){
+		}
+		else if (type == "UI_D")
+		{
 			return (url = this.CGPlusUrl + '/REST/UI/' + api+ '?' + format)
 		}
 	}
+
+	//------------------------------------------------------------------------------------------------
+	//  Fetch Api manager function, we implement this to handle all the fetch request and stop
+	//  them if necessary
+	//------------------------------------------------------------------------------------------------
+
+	ExecuteRequest = async (url, toJson) => {
+
+		// Default toJson value to true if not provided
+		const returnJson = toJson !== undefined ? toJson : false;
+	
+		// Here we check if we got the configuration
+		if (this.CGPlusIp && this.CGPlusPort ) {
+			try {
+				const res = await fetch(url);
+	
+				if (res.ok) {
+					// Return JSON data on success if returnJson is true
+					if (returnJson) {
+						return await res.json();
+					} else {
+						return res; // Return the entire response object
+					}
+				} else {
+					// Log an error on failure
+					this.LogManager('error', 'Error fetching data from server:' + res.statusText);
+				}
+			} catch (err) {
+				// Handle network error
+				this.LogManager('error', 'Network error occurred:' + err.message);
+			}
+		} else {
+			// No configuration
+			this.LogManager('warn', 'No configuration');
+		}
+	
+		// Return null if there is no data to return
+		return null;
+	}
+
 
 	//------------------------------------------------------------------------------------------------
 	//  function called when we initilialize the module, since the dynamic
@@ -55,25 +122,29 @@ class CGPlus {
 	//  if a connection is extablished, if not return error that machine wasn't reached
 	//------------------------------------------------------------------------------------------------
 
-	Connect = async () => {
+	Connect = async () => 
+	{
 		await this.loadFetch()
 		//this.debug('debug', 'CONNECT')
-
 		url = this.getUrl('IsAlive', 'data_no_p')
-		console.log('res url', url)
-		try {
-			const response = await fetch(url)
+		
+		try
+		{
+			const response = await this.ExecuteRequest(url,false)
 
-			//console.log('res Connect', response);s
-
-			if (response.status == 200) {
-				console.log('successful CG connection')
+			if (response) 
+			{
+				this.LogManager('info', 'successful CG connection');
 				return true
-			} else {
-				console.log('rejected CG connection')
+			} 
+			else 
+			{
+				this.LogManager('warn', 'rejected CG connection');
 			}
-		} catch (err) {
-			console.error('Error in Connect:', err)
+		}
+		catch (err) 
+		{
+			this.LogManager('error', 'Error in Connect:', err.message);
 			return false
 		}
 	}
